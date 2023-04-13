064369df9eb54d0ca7e332da6b119c60b8659d66
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-04-13 18:56:25 +0200

    price and name system done

--- js/script.js
+++ js/script.js
@@ -27,17 +27,49 @@
 //     document.getElementById("ticket").innerHTML = prezzo.toFixed(2)
 // }
 
-let NameSurname = document.querySelector(".nome-cognome")
-const val = NameSurname
-function getVal(NameSurname) {
-  const val = NameSurname.value;
-  console.log(val);
+
+let nome = document.querySelector(".name").value
+let numeric_km = document.querySelector(".km").value
+let age = document.querySelector(".età").value
+
+
+const price_per_km = 0.21
+const prezzo = numeric_km * price_per_km
+
+let sconto_20 = (prezzo - (prezzo * 20) / 100).toFixed(2);
+let sconto_40 = (prezzo - (prezzo * 40) / 100).toFixed(2);
+
+const btn = document.querySelector(".genera")
+btn.addEventListener("click" , function(){
+    let nome = document.querySelector(".name").value
+    document.getElementById("showname").innerHTML = nome
+    console.log(nome)
+
+    let numeric_km = document.querySelector(".km").value
+    console.log(numeric_km)
+
+    let age = document.querySelector(".età").value
+    console.log(age)
+
+    if(age < 18){
+        const price_per_km = 0.21
+        const prezzo = numeric_km * price_per_km
+        let sconto_20 = (prezzo - (prezzo * 20) / 100).toFixed(2);
+        document.getElementById("price").innerHTML = sconto_20 + " &euro;"
+        document.getElementById("offerta").innerHTML = "Sconto minori"
+    }else if (age > 65){
+        const price_per_km = 0.21
+        const prezzo = numeric_km * price_per_km
+        let sconto_40 = (prezzo - (prezzo * 40) / 100).toFixed(2);
+        document.getElementById("price").innerHTML = sconto_40 + " &euro;"
+        document.getElementById("offerta").innerHTML = "Sconto over 65"
+    }else{
+        const price_per_km = 0.21
+        const prezzo = numeric_km * price_per_km
+        document.getElementById("price").innerHTML = prezzo + " &euro;"
+        document.getElementById("offerta").innerHTML = "Tariffa Standard"
+    }
 }
 
-const eleBtnGenera = document.querySelector(".genera")
 
-eleBtnGenera.addEventListener("click", function(){
-    console.log("mi hai cliccato")
-    document.getElementById("infouno").innerHTML = NameSurname
-}
 )
